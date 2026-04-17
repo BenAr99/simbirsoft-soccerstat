7721e2733200d2e0916f6cc53bf1ab060c29c8f4
BenAr99 <107556315+BenAr99@users.noreply.github.com>
2026-04-17 18:05:33 +0400

    Update auth-token.interceptor.ts

--- src/app/interceptors/auth-token.interceptor.ts
+++ src/app/interceptors/auth-token.interceptor.ts
@@ -2,10 +2,12 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '@environments/environment';
 
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = environment.apiKey;
-
   if (req.url.includes('api.football-data.org')) {
+    const token = environment.apiKey;
+    const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/' + req.url;
+
     const modifiedRequest = req.clone({
+      url: corsProxyUrl,
       setHeaders: {
         'X-Auth-Token': token,
       },
